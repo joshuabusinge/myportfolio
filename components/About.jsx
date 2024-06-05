681fae8960a55dc8a683b8bd49e4cfaf0b247a65
JOSHUA BUSINGE <joshuabujo9@gmail.com>
2024-06-05 22:59:45 +0300

    First Commit

---
+++ components/About.jsx
@@ -0,0 +1,19 @@
+import DevImg from "./DevImg";
+import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import {
+  User2,
+  MailIcon,
+  HomeIcon,
+  PhoneCall,
+  GraduationCap,
+  Calendar,
+  Briefcase,
+} from "lucide-react";
+
+const About = () => {
+  return <div>About</div>;
+};
+
+export default About;
