945881d0f2e8758eb61584c4ab93c36fda7c485c
JOSHUA BUSINGE <joshuabujo9@gmail.com>
2024-06-06 21:38:50 +0300

    About Me Updates

--- components/About.jsx
+++ components/About.jsx
@@ -12,8 +12,197 @@ import {
   Briefcase,
 } from "lucide-react";
 
+const infoData = [
+  {
+    icon: <User2 size={20} />,
+    text: "Joshua Businge",
+  },
+  {
+    icon: <PhoneCall size={20} />,
+    text: "+256-701293950",
+  },
+  {
+    icon: <MailIcon size={20} />,
+    text: "joshuabujo9@gmail.com",
+  },
+  {
+    icon: <Calendar size={20} />,
+    text: "Born on 8th Feb, 1993",
+  },
+  {
+    icon: <GraduationCap size={20} />,
+    text: "Masters of Science in Computer Science",
+  },
+  {
+    icon: <HomeIcon size={20} />,
+    text: "Plot 120 Semawata RD - Ntinda Kampala(U)",
+  },
+];
+
+const qualificationData = [
+  {
+    title: "education",
+    data: [
+      {
+        university: "Makerere University",
+        qualification: "Bachelor of Science in Computer Science",
+        years: "2012-2016",
+      },
+      {
+        university: "Makerere University",
+        qualification: "Masters of Science in Computer Science",
+        years: "2022-2024",
+      },
+    ],
+  },
+  {
+    title: "experience",
+    data: [
+      {
+        organisation: "ThinkIT Limited",
+        role: "Co-founder and Director",
+        years: "2013-Present",
+      },
+      {
+        organisation: "ThinkIT Limited",
+        role: "Data Scientist",
+        years: "2016-Present",
+      },
+      {
+        organisation: "ResilientAfrica Network",
+        role: "Innovator Matibabu Project",
+        years: "2013-2017",
+      },
+      {
+        organisation: "UNFPA",
+        role: "Technical Developer",
+        years: "2016-2018",
+      },
+    ],
+  },
+];
+
+const skillData = [
+  {
+    title: "skills",
+    data: [
+      {
+        name: "Python,SQL",
+      },
+      {
+        name: "Data Science and Data Analysis",
+      },
+      {
+        name: "NextJs, Tailwind",
+      },
+      {
+        name: "Front-End Development",
+      },
+      {
+        name: "Microsoft Azure",
+      },
+      {
+        name: "Cloud Practioner Certificate",
+      },
+    ],
+  },
+  {
+    title: "tools",
+    data: [
+      {
+        imgPath: "/about/vscode.svg",
+      },
+      {
+        imgPath: "/about/figma.svg",
+      },
+      {
+        imgPath: "/about/notion.svg",
+      },
+      {
+        imgPath: "/about/wordpress.svg",
+      },
+    ],
+  },
+];
+
 const About = () => {
-  return <div>About</div>;
+  const getData = (arr, title) => {
+    return arr.find((item) => item.title === title);
+  };
+
+  console.log(getData(qualificationData, "education"));
+  return (
+    <section className="xl:h-[860px] pb-12 xl:py-24">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+          About me
+        </h2>
+        <div className="flex flex-col xl:flex-row">
+          {/* {image} */}
+          <div className="hidden xl:flex flex-1 relative">
+            <DevImg
+              containerStyles="bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative"
+              imgSrc="/about/developer.png"
+            />
+          </div>
+          {/* {tabs} */}
+
+          <div className="flex-1">
+            <Tabs defaultValue="personal">
+              <TabsList className="w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none">
+                <TabsTrigger className="w-[162px] xl:w-auto" value="personal">
+                  Personal Info
+                </TabsTrigger>
+                <TabsTrigger
+                  className="w-[162px] xl:w-auto"
+                  value="qualification"
+                >
+                  Qualifications
+                </TabsTrigger>
+                <TabsTrigger className="w-[162px] xl:w-auto" value="skills">
+                  Skills
+                </TabsTrigger>
+              </TabsList>
+              {/* {tabs content} */}
+              <div className="text-lg mt-12 xl:mt-8">
+                {/* {personal} */}
+                <TabsContent value="personal">
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-4">
+                      Unmatched Service Quality for Over 10 Years
+                    </h3>
+                    <p className="subtitle max-w-xl mx-auto xl:mx-0">
+                      I specialise in crafting intuitive websites with
+                      cutting-edge technology, delivering and engaging user
+                      experiences.
+                    </p>
+                    {/* {icons} */}
+                    <div className="grid xl:grid-cols-2 gap-4 mb-12">
+                      {infoData.map((item, index) => {
+                        return (
+                          <div
+                            className="flex items-center gap-x-4 mx-auto xl:mx-0"
+                            key={index}
+                          >
+                            <div className="text-primary">{item.icon}</div>
+                            <div>{item.text}</div>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </TabsContent>
+                <TabsContent value="qualification">
+                  qualification info
+                </TabsContent>
+                <TabsContent value="skills">skills info</TabsContent>
+              </div>
+            </Tabs>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
