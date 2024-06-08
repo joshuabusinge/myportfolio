0e16dfa31d6321db2b7b3a5fefd72c0315a9530a
JOSHUA BUSINGE <joshuabujo9@gmail.com>
2024-06-08 22:13:53 +0300

    Services Page

---
+++ components/Services.jsx
@@ -0,0 +1,66 @@
+import { GanttChartSquare, Blocks, Gem } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const servicesData = [
+  {
+    icon: <GanttChartSquare size={72} strokeWidth={0.8} />,
+    title: "Data Analysis",
+    description:
+      " Using Jupyter Notebook and Data bricks to Extract, Transform and Load data.",
+  },
+  {
+    icon: <Blocks size={72} strokeWidth={0.8} />,
+    title: "Data Science",
+    description:
+      " Applying Machine Learning and Large language models to automate tasks using data.",
+  },
+  {
+    icon: <Gem size={72} strokeWidth={0.8} />,
+    title: "Web Development",
+    description:
+      " Using NextJs, Tailwind CSS, Shadcn UI to design and develop web platforms. ",
+  },
+];
+
+const Services = () => {
+  return (
+    <section className="mb-12 xl:mb-36">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-12 xl:mb-24 text-center mx-auto">
+          My Services
+        </h2>
+        {/* {grid items} */}
+        <div className="grid xl:grid-cols-3 justify-center gap-y-12 xl:gap-y-24">
+          {servicesData.map((item, index) => {
+            return (
+              <Card
+                className="w-full max-w-[424px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative"
+                key={index}
+              >
+                <CardHeader className=" text-primary absolute -top-[60px]">
+                  <div className="w-[140px] h-[80px] bg-white dark:bg-background flex justify-center items-center">
+                    {item.icon}
+                  </div>
+                </CardHeader>
+                <CardContent className="text-center">
+                  <CardTitle className="mb-4">{item.title} </CardTitle>
+                  <CardDescription className="text-lg">
+                    {item.description}
+                  </CardDescription>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
