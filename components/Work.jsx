6c371dfb8c7a4e7079ffc2d9ea440338a5ebd8e2
JOSHUA BUSINGE <joshuabujo9@gmail.com>
2024-06-10 23:37:11 +0300

    Create ProjectCard

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,5 +1,60 @@
+"use client";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+//import swiper styles
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+//import required modules
+import { Pagination } from "swiper/modules";
+
+//components
+import ProjectCard from "@/components/ProjectCard";
+
+const projectData = [
+  {
+    image: "/work/3.png",
+    category: "next js",
+    name: "Nexa Website",
+    description: "This is the project am working in the commenting about",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "next js",
+    name: "Serenity Website",
+    description: "This is the project am working in the commenting about",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/4.png",
+    category: "next js",
+    name: "Nova Website",
+    description: "This is the project am working in the commenting about",
+    link: "/",
+    github: "/",
+  },
+];
+
 const Work = () => {
+  return (
+    <section>
+      <div className="container mx-auto">
+        {/* {text} */}
+        <div>
+          <h2 className="section-title mb-4">Latest Projects</h2>
+          <p>Heyyyy this is container a latest projects. </p>
+          <Link href="/projects">
+            <Button>All projects</Button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
   return <div>Work</div>;
 };
-
 export default Work;
