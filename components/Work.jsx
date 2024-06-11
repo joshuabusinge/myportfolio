d6b7ab98b19898719a730f92eb046b3349ad6435
JOSHUA BUSINGE <joshuabujo9@gmail.com>
2024-06-12 00:11:41 +0300

    Updated the Work component

--- components/Work.jsx
+++ components/Work.jsx
@@ -2,6 +2,9 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 
+//import swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
 //import swiper styles
 
 import "swiper/css";
@@ -38,19 +41,61 @@ const projectData = [
     link: "/",
     github: "/",
   },
+  {
+    image: "/work/2.png",
+    category: "next js",
+    name: "Serenity Website",
+    description: "This is the project am working in the commenting about",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/4.png",
+    category: "next js",
+    name: "Nova Website",
+    description: "This is the project am working in the commenting about",
+    link: "/",
+    github: "/",
+  },
 ];
 
 const Work = () => {
   return (
-    <section>
+    <section className="relative mb-12 xl:mb-48">
       <div className="container mx-auto">
         {/* {text} */}
-        <div>
+        <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
           <h2 className="section-title mb-4">Latest Projects</h2>
-          <p>Heyyyy this is container a latest projects. </p>
+          <p className="subtitle mb-8">
+            Heyyyy this is container a latest projects.
+          </p>
           <Link href="/projects">
             <Button>All projects</Button>
           </Link>
+        </div>
+        {/* {slider} */}
+        <div className="xl:max-w-[1000px] xl:absolute right-0 top-0">
+          <Swiper
+            className="h-[480px]"
+            slidesPerView={1}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+              },
+            }}
+            spaceBetween={30}
+            modules={[Pagination]}
+            pagination={{ clickable: true }}
+          >
+            {/* {show only the first 4 projects for the slides} */}
+            {projectData.slice(0, 4).map((project, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <ProjectCard project={project} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
         </div>
       </div>
     </section>
