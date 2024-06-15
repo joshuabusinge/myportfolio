4a22639f20ecc1b5e45f4dbb6a41cf31fee95098
JOSHUA BUSINGE <joshuabujo9@gmail.com>
2024-06-15 17:02:49 +0300

    Added the filter for all projects categories

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -57,6 +57,14 @@ const Projects = () => {
   const [categories, setCategories] = useState(uniqueCategories);
   const [category, setCategory] = useState("all projects");
   // console.log(category);
+
+  const filteredProjects = projectData.filter((project) => {
+    return category === "all projects"
+      ? project
+      : project.category === category;
+  });
+
+  // console.log(filteredProjects);
   return (
     <section className="min-h-screen pt-12">
       <div className="container mx-auto">
@@ -64,7 +72,7 @@ const Projects = () => {
           My Projects
         </h2>
         {/* {tabs} */}
-        <Tabs defaultValue={category}>
+        <Tabs defaultValue={category} className="mb-24 xl:mb-16">
           <TabsList className="w-full grid h-full md:grid-cols-4 lg:max-w-[640px] mb-12 mx-auto md:border dark:broder-none">
             {categories.map((category, index) => {
               return (
@@ -79,6 +87,16 @@ const Projects = () => {
               );
             })}
           </TabsList>
+          {/* {tabs content} */}
+          <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {filteredProjects.map((project, index) => {
+              return (
+                <TabsContent value={category} key={index}>
+                  <ProjectCard project={project} />
+                </TabsContent>
+              );
+            })}
+          </div>
         </Tabs>
       </div>
     </section>
